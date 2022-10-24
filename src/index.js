1486bee6d00e5378ad6f01d10cd1b925ced862bc
Danelle <rhowia@gmail.com>
2022-10-24 13:39:50 -0400

    Coding files to display a webpage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import _ from 'lodash';
 import './style.css';
-import printMe from './print.js';
 
-function component() {
-    
-    const element = document.createElement('container');
-    // element.classList.add('content');
-    const navBar = document.createElement('nav');
-    
-    element.appendChild(navBar);
-    element.textContent = 'Testing, testing';
+function createHeader() {
+    const header = document.createElement("header");
+    header.setAttribute('id', 'header');
 
-    return element;
+    const restaurantName = document.createElement("h1");
+    restaurantName.classList.add("restaurant-name");
+    restaurantName.textContent = "Whatever";
+
+    header.appendChild(restaurantName);
+    header.appendChild(createNav());
+    
+    return header;
 }
 
-document.body.appendChild(component());
+function createNav() {
+    const nav = document.createElement('nav');
+    nav.setAttribute('id', 'navigation');
+
+    const list = document.createElement('ul');
+    list.classList.add('nav-ul');
+    nav.appendChild(list);
+
+    const listItems = ['Menu', 'LOGO', 'Contact'];
+    
+    for (const item of listItems) {
+        const li = document.createElement('li');
+        li.classList.add('nav-li');
+        li.textContent = item;
+        list.appendChild(li);
+    }
+
+    return nav;
+}
+
+function createMain() {
+    const main = document.createElement("main");
+    main.setAttribute('id', 'main');
+
+    main.textContent = "Whatever you want to eat, we have it - maybe";
+
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement("footer");
+    footer.setAttribute('id', 'footer');
+
+    const copyright = document.createElement('p');
+    copyright.textContent = `Copyright ${new Date().getFullYear()}`;
+
+    footer.appendChild(copyright);
+
+    return footer;
+}
+
+function createWebpage() {
+    
+    const webpage = document.createElement('div');
+    webpage.setAttribute('id', 'container');
+
+    webpage.appendChild(createHeader());
+    webpage.appendChild(createMain());
+    webpage.appendChild(createFooter());
+
+    return webpage;
+}
+
+document.body.appendChild(createWebpage());
